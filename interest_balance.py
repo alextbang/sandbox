655052aaaa0ad4d034daed232b79e3f416eef283
alexbang <abang@mitre.org>
2024-03-07 08:07:55 -0500

    fix reduced interest var

--- interest_balance.py
+++ interest_balance.py
@@ -1,43 +1,40 @@
 
-# # calculate total interest paid over the remainder of mortgage payoff period
+# calculate total interest paid over the remainder of mortgage payoff period
+
+print("Mortgage interest balance")
 #
+# number of months remaining to pay off mortgage
+months = 78
+#
+# initial interest paid starting month, decreases monthly
+interest = 639
+
+# total interest paid over time
+total = interest
+ 
+while months > 0:
+    interest-=8
+    months-=1
+    print("interest balance:", interest)
+    total += interest
+    #print(f"interest total: {total}")
+ 
+print("Total interest paid:", total)
+
+######################################
+ 
 # print("Mortgage interest balance")
-# #
-# # number of months remaining to pay off mortgage
-# months = 78
-# #
-# # amount of intest paid each month, decreases monthly
-
+# months = 30
 # interest = 639
-
 # total = interest
  
 # while months > 0:
-#     diff = interest - 8
-#     interest = diff
+#     interest-=8
 #     months-=1
-#     # total = interest + diff
 #     print("interest balance:", interest)
 #     total += interest
 #     #print(f"interest total: {total}")
  
 # print("Total intrest paid:", total)
 
-######################################
- 
-print("Mortgage interest balance")
-months = 30
-interest = 639
-total = interest
- 
-while months > 0:
-    diff = interest - 8
-    interest = diff
-    months-=1
-    # total = interest + diff
-    print("interest balance:", interest)
-    total += interest
-    #print(f"interest total: {total}")
- 
-print("Total intrest paid:", total)
 ######################################
